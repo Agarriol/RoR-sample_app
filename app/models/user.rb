52ba3aedb82156f3e07b5f439ed7c62c48294044
agarriol <agarriol@everis.com>
2018-02-02 12:05:39 +0100

    Implements advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   before_save { self.email = email.downcase }
@@ -12,9 +15,34 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   # Returns the hash digest of the given string.
-  def User.digest(string)
+  #Sirve para hace run hash de las contraseñas y guardas este en db
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # Returns a random token.
+  #Para recordar al usuario
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+#Recuerda esta sesión del usuario
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 end
